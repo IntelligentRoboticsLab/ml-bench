c9383602a809b883bff7cdd3fe8040f10fa65183
Gijs de Jong <berichtaangijs@gmail.com>
2024-10-25 14:47:35 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,32 @@
+# ml-bench 🔬
+
+A tool for benchmarking inference time of onnx models using the
+[OpenVINO](https://software.intel.com/content/www/us/en/develop/tools/openvino-toolkit.html) runtime.
+
+The benchmarks are run using [criterion](https://github.com/bheisler/criterion.rs), running a forward pass on the model
+`--samples` times. Once the benchmark is complete, the results are printed to stdout.
+
+
+## Usage
+
+```sh
+./ml-bench <model> [--samples <num_samples>]
+```
+
+## Building
+
+### macOS
+
+When compiling from macOS to `x86-64-unknown-linux-gnu`, create a `.cargo/config.toml` file with the following content:
+
+```toml
+[target.x86_64-unknown-linux-gnu]
+linker = "x86_64-unknown-linux-gnu-gcc"
+ar = "x86_64-unknown-linux-gnu-ar"
+
+[env]
+TARGET_CC = "x86_64-unknown-linux-gnu-gcc"
+TARGET_CXX = "x86_64-unknown-linux-gnu-g++"
+CC = "/opt/homebrew/opt/x86_64-unknown-linux-gnu/bin/x86_64-linux-gnu-gcc"
+CXX = "/opt/homebrew/opt/x86_64-unknown-linux-gnu/bin/x86_64-linux-gnu-g++"
+```
--- src/benchmark.rs
+++ src/benchmark.rs
@@ -1,12 +1,16 @@
+use std::path::Path;
+
 use criterion::Criterion;
-use miette::{Context, IntoDiagnostic, Result};
+use miette::{miette, Context, IntoDiagnostic, Result};
 use openvino::{CompiledModel, InferRequest};
 
-fn load_model(path: impl AsRef<str>) -> Result<CompiledModel> {
+fn load_model(path: impl AsRef<Path>) -> Result<CompiledModel> {
     let mut core = openvino::Core::new().into_diagnostic()?;
-    let model = core
-        .read_model_from_file(path.as_ref(), path.as_ref())
-        .into_diagnostic()?;
+    let onnx_path = path
+        .as_ref()
+        .to_str()
+        .ok_or_else(|| miette!("Model file path is not valid UTF-8"))?;
+    let model = core.read_model_from_file(onnx_path, "").into_diagnostic()?;
     core.compile_model(&model, openvino::DeviceType::CPU)
         .into_diagnostic()
 }
@@ -33,10 +37,15 @@ fn create_dummy_infer_request(model: &mut CompiledModel) -> Result<InferRequest>
 }
 
 /// Run a benchmark on an ONNX model.
-pub fn run_benchmark_onnx(path: impl AsRef<str>, c: &mut Criterion) -> Result<()> {
-    let mut model = load_model(path).context("failed to load model")?;
+pub fn run_benchmark_onnx(path: impl AsRef<Path>, c: &mut Criterion) -> Result<()> {
+    let mut model = load_model(&path).context("failed to load model")?;
+    let onnx_name = path
+        .as_ref()
+        .file_name()
+        .and_then(|s| s.to_str())
+        .ok_or_else(|| miette!("Model file name is not valid UTF-8"))?;
 
-    c.bench_function("onnx inference", |b| {
+    c.bench_function(onnx_name, |b| {
         b.iter(|| {
             let mut request =
                 create_dummy_infer_request(&mut model).expect("failed to create inference request");
