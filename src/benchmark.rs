116a8b73db44039794ba97b5c5295efbcdffba1e
Gijs de Jong <berichtaangijs@gmail.com>
2024-12-05 12:06:33 +0100

    Only benchmark model inference

--- src/benchmark.rs
+++ src/benchmark.rs
@@ -1,8 +1,8 @@
 use std::path::Path;
 
-use criterion::Criterion;
+use criterion::{black_box, Criterion};
 use miette::{miette, Context, IntoDiagnostic, Result};
-use openvino::{CompiledModel, InferRequest};
+use openvino::CompiledModel;
 
 fn load_model(path: impl AsRef<Path>) -> Result<CompiledModel> {
     let mut core = openvino::Core::new().into_diagnostic()?;
@@ -15,27 +15,6 @@ fn load_model(path: impl AsRef<Path>) -> Result<CompiledModel> {
         .into_diagnostic()
 }
 
-/// Create a dummy infer request with zero-filled input tensors.
-#[inline]
-fn create_dummy_infer_request(model: &mut CompiledModel) -> Result<InferRequest> {
-    let mut request = model.create_infer_request().into_diagnostic()?;
-    let num_inputs = model.get_input_size().into_diagnostic()?;
-
-    for i in 0..num_inputs {
-        let input_node = model.get_input_by_index(i).into_diagnostic()?;
-        let shape = input_node.get_shape().into_diagnostic()?;
-        let element_type = input_node.get_element_type().into_diagnostic()?;
-
-        let tensor = openvino::Tensor::new(element_type, &shape).into_diagnostic()?;
-
-        request
-            .set_input_tensor_by_index(i, &tensor)
-            .into_diagnostic()?;
-    }
-
-    Ok(request)
-}
-
 /// Run a benchmark on an ONNX model.
 pub fn run_benchmark_onnx(path: impl AsRef<Path>, c: &mut Criterion) -> Result<()> {
     let mut model = load_model(&path).context("failed to load model")?;
@@ -43,14 +22,17 @@ pub fn run_benchmark_onnx(path: impl AsRef<Path>, c: &mut Criterion) -> Result<(
         .as_ref()
         .file_name()
         .and_then(|s| s.to_str())
-        .ok_or_else(|| miette!("Model file name is not valid UTF-8"))?;
+        .ok_or_else(|| miette!("model file name is not valid UTF-8"))?;
 
     c.bench_function(onnx_name, |b| {
         b.iter(|| {
-            let mut request =
-                create_dummy_infer_request(&mut model).expect("failed to create inference request");
+            let request = model
+                .create_infer_request()
+                .expect("failed to create inference request");
 
-            request.infer().expect("failed to complete inference");
+            black_box(request)
+                .infer()
+                .expect("failed to complete inference");
         });
     });
 
